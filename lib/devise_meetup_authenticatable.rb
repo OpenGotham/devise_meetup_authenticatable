4dbd6ab870fd28b5a160a28bae3383fc48bd398c
matthew jording <mjording@opengotham.com>
2010-07-31 11:13:25 -0400

    patch

--- lib/devise_meetup_authenticatable.rb
+++ lib/devise_meetup_authenticatable.rb
@@ -1,8 +1,12 @@
-require 'devise'
 
-require 'devise_meetup_authenticatable/schema'
+require 'devise'
+require 'oauth'
+
+require 'devise_meetup_authenticatable/model'
 require 'devise_meetup_authenticatable/strategy'
+require 'devise_meetup_authenticatable/schema'
 require 'devise_meetup_authenticatable/routes'
+
 
 Devise.add_module(:meetup_authenticatable,
   :strategy => true,
