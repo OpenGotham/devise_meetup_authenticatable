13861479c875c50ec9fd6f7d87f83ec215590866
matthew jording <mjording@opengotham.com>
2010-07-31 11:19:35 -0400

    patch

--- lib/devise_meetup_authenticatable/routes.rb
+++ lib/devise_meetup_authenticatable/routes.rb
@@ -1,7 +1,3 @@
   ActionDispatch::Routing::Mapper.class_eval do
-    
-   # map.after_login 'oauth_complete', :controller => 'meetup_callback', :action => 'after_login'
-   alias_method :oauth_complete, :devise_session
+    alias_method :meetup_complete, :devise_session
   end
-
-end
